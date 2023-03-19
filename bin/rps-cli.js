75be3d28b022513674c688080bb05b1a829ccfae
Muktha Santhoshkumar <muktha@email.unc.edu>
2023-03-19 11:25:16 -0400

    helpmsg

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,4 +1,30 @@
 #!/usr/bin/env node
-import { rps } from '../lib/rpsls.js'; 
+import {rps} from '../lib/rpsls.js'; 
+import minimist from "minimist"; 
 
 const args = minimist(process.argv.slice(2)); 
+
+if (args.h || args.help) {
+   displayHelpMsg(); 
+   process.exit(0); 
+} 
+
+if (args.r || args.rules) {
+   displayRuleMsg(); 
+   process.exit(0); 
+} 
+
+function displayHelpMsg() {
+   console.log('Usage: node-rps [SHOT]
+Play Rock Paper Scissors (RPS)
+
+  -h, --help      display this help message and exit
+  -r, --rules     display the rules and exit
+
+Examples:
+  node-rps        Return JSON with single player RPS result.
+                  e.g. {"player":"rock"}
+  node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                  e.g {"player":"rock","opponent":"scissors","result":"win"}')
+
+} 
