b9e2d86947131e849594b037f9243504d683e88f
Muktha Santhoshkumar <muktha@email.unc.edu>
2023-03-19 23:00:26 -0400

    fixed brackets & msgs

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -19,31 +19,30 @@ try {
    if (result != 'error') {
 	console.log(JSON.stringify(output)); 
    } else {
-	  console.error('${args._[0] is out of range.'); 
+	  console.error('${args._[0]} is out of range.'); 
 	  displayHelpMsg(); 
 	  displayRuleMsg(); 
 } 
     
 
 function displayHelpMsg() {
-   console.log('Usage: node-rps [SHOT]
-   Play Rock Paper Scissors (RPS)
-
-     -h, --help      display this help message and exit
-     -r, --rules     display the rules and exit
-
-   Examples:
-     node-rps        Return JSON with single player RPS result.
-                     e.g. {"player":"rock"}
-     node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-                     e.g {"player":"rock","opponent":"scissors","result":"win"}')
+    console.log('Usage: node-rps [SHOT]
+    Play Rock Paper Scissors (RPS)
+    
+      -h, --help      display this help message and exit
+      -r, --rules     display the rules and exit
+    
+    Examples:
+      node-rps        Return JSON with single player RPS result.
+                      e.g. {"player":"rock"}
+      node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                      e.g {"player":"rock","opponent":"scissors","result":"win"}')
 } 
 
 function displayRuleMsg() {
-   console.log('Rules for Rock Paper Scissors:
+    console.log('Rules for Rock Paper Scissors:
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock CRUSHES Scissors')
 
-  - Scissors CUTS Paper
-  - Paper COVERS Rock
-  - Rock CRUSHES Scissors')
-
-} 
+}
